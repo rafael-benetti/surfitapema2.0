28e3b84602126fcd9204460e989850498f80523a
rafael-benetti <dssweb5@gmail.com>
2022-05-12 22:02:31 -0300

    up

---
+++ build/credit_card_input_form/generated/source/buildConfig/debug/com/origogi/credit_card_input_form/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.origogi.credit_card_input_form;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.origogi.credit_card_input_form";
+  public static final String BUILD_TYPE = "debug";
+}
---
+++ lib/config.dart
@@ -0,0 +1,19 @@
+class Config {
+  //set your api server url here
+  static String apiServerUrl              = "http://demo.redtvlive.com/oxoo/v13/rest-api/";
+  //set your api key here
+  static String apiKey                    = "spagreen";
+  //set your onesignalID here
+  static String oneSignalID               = "xxxxx-xxxx-xxxx-xxxx-xxxxxxx";
+  //set term and conditaions url here
+  static String termsPolicyUrl            = "https://spagreen.net/tearms-of-services/";
+
+
+
+  static final bool enableFacebookAuth    = true;
+  static final bool enableGoogleAuth      = true;
+  static final bool enablePhoneAuth       = true;
+  static final bool enableAppleAuthForIOS = true;
+  //publicKeyBase64 from play store to implement in app purchase
+  static final String publicKeyBase64     ="xxxxxxx/xxx+xxx+xxx+xx+xxxx+xxx+xx+xxxxx+xxx/x56/W+xxx+xx+xx/xx";
+}
